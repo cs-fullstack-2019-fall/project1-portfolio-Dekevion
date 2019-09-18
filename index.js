58f8a3840ee1a156347a9df8a553a014033580ba
Dekevion Wade <Dekevionwade@yahoo.com>
2019-09-18 14:53:13 -0500

    answers

---
+++ index.js
@@ -0,0 +1,41 @@
+let frame = document.getElementById('Frameworks');
+let Projects = document.getElementById('Projects');
+let Education = document.getElementById('Education');
+let Reflection = document.getElementById('Reflections');
+let Contact = document.getElementById('Contact');
+let Resume = document.getElementById('Resume');
+
+
+
+
+
+//
+// frame.addEventListener('mouseenter',function () {
+//
+// });
+
+
+// Projects.addEventListener('click',function () {
+//     alert('Click to Learn more about my Passion Project, Rumy')
+// });
+//
+//
+// Education.addEventListener('click',function () {
+//     alert('you got mail')
+// });
+//
+//
+// Reflection.addEventListener('click',function () {
+//     alert('you got mail')
+// });
+//
+//
+// Contact.addEventListener('click',function () {
+//     alert('you got mail')
+// });
+//
+//
+//
+// Resume.addEventListener('click',function () {
+//     alert('you got mail')
+// });
