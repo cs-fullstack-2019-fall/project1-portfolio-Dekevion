b51e0856525949e7c036c0c33ad3867119ef7794
Dekevion Wade <Dekevionwade@yahoo.com>
2020-01-23 01:37:14 -0600

    prettified website

---
+++ my_baby.js
@@ -0,0 +1,8 @@
+// function fadeIn () {
+//     let elem = document.getElementById("content_box");
+//     elem.style.opacity = 1;
+//     elem.style.transition = "opacity 1s";
+//     elem.addEventListener("click",fadeIn)
+//     // elem.style.visibility = "visible";
+//     console.log('this')
+// }
